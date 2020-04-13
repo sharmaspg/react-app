ee13c65a406605f7a0d51ec6076ad1c80739fe60
sharma <sharmaspg1995@gmail.com>
2020-04-13 12:32:27 +0530

    Updates

---
+++ precache-manifest.bdb4ce621aa1bd662b101caaba3d7b29.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f09bb9be6d3eeb59f86b634f3e6c5b3",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "51850db75ecc7176e6af",
+    "url": "/react-app/static/css/2.9de2e04a.chunk.css"
+  },
+  {
+    "revision": "ea561009b5f206c96f0f",
+    "url": "/react-app/static/css/main.8204e906.chunk.css"
+  },
+  {
+    "revision": "51850db75ecc7176e6af",
+    "url": "/react-app/static/js/2.f7c7cccd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-app/static/js/2.f7c7cccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea561009b5f206c96f0f",
+    "url": "/react-app/static/js/main.4ed470b7.chunk.js"
+  },
+  {
+    "revision": "1dc033653be254afbc4b",
+    "url": "/react-app/static/js/runtime-main.068afa05.js"
+  },
+  {
+    "revision": "4e73cf1c774c0f3beaccc514997860a0",
+    "url": "/react-app/static/media/ac.4e73cf1c.png"
+  },
+  {
+    "revision": "7732f65c2a00a9c7a74faa8c5af014e4",
+    "url": "/react-app/static/media/lamp.7732f65c.png"
+  },
+  {
+    "revision": "8a8f653d3b161de0779e4a0a35c2b1a8",
+    "url": "/react-app/static/media/lock.8a8f653d.png"
+  },
+  {
+    "revision": "4c7bae7ae93277817271b621a8c29450",
+    "url": "/react-app/static/media/thermostat.4c7bae7a.png"
+  },
+  {
+    "revision": "48edf815a195ca8de3f4eed7b708a323",
+    "url": "/react-app/static/media/tv.48edf815.png"
+  }
+]);
